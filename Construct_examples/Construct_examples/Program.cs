8bd4a8fecd466089e4151ad32997b74ee74aa060
Alexander Roikier Møller <alex6308@elev.tec.dk>
2018-08-16 15:12:32 +0200

    <Færdigt resultat, dog uden "enum">

---
+++ Construct_examples/Construct_examples/Program.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Construct_examples
+{
+    class Program
+    {
+        struct Account
+        {
+            public string Navn;
+            public string cprNummer;
+            public double saldo;
+        }
+        struct Beer
+        {
+            public string fabrikat;
+            public string type;
+            public double procent;
+            public double volumen;
+        }
+        static void Main(string[] args)
+        {
+            //Opret en struct Account indeholdende Navn, CPR-nr og Saldo. 
+            //Opret et par variable af Account-typen, giv dem værdier og udskriv dem.
+            //Opret en metode UdskrivAccount(), som modtager et Account-objekt som parameter og udskriver dens værdier. 
+
+            
+            Account a1;
+            a1.Navn = "Bo";
+            a1.cprNummer = "16082018-1122";
+            a1.saldo = 18000;
+
+            Account a2;
+            a2.Navn = "Tim";
+            a2.cprNummer = "17082018-1122";
+            a2.saldo = 10000;
+
+            Beer b1;
+            b1.fabrikat = "Tuborg";
+            b1.type = "Øl";
+            b1.procent = 4.2;
+            b1.volumen = 33.3;
+
+            string udskriv(Account bo)
+            {
+                return "navn : " + bo.Navn + " | " + "cpr-nr : " + bo.cprNummer + " | " + "saldo : " + bo.saldo;
+            }
+            string udskriv1(Beer øl)
+            {
+                return "Fabrikat : " + b1.fabrikat + " | " + "Type : " + b1.type + " | " + "Procent : " + b1.procent + " %" + " | " + "Volumen : " + b1.volumen + " cl";
+            }
+
+            Console.WriteLine(udskriv(a1));
+            Console.WriteLine(udskriv(a2));
+            Console.WriteLine(udskriv1(b1));
+
+            //Console.WriteLine("Navn : {0} | Cpr-Nummer : {1} | Saldo : {2}kr.",a1.Navn, a1.cprNummer, a1.saldo);
+            Console.ReadKey();
+        }
+    }
+}
